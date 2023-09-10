a3fca387d0c0af46d91c3caaf9d067a1fcf8b444
Hanf Chase <88610657+hanfeijiang@users.noreply.github.com>
2023-09-10 09:32:04 +0800

    Added detailed comments explaining Changed variable and function names to English Changed print outputs to English Removed unused json loading code

--- Unets-2D/utils/tools.py
+++ Unets-2D/utils/tools.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
+
 from torch import nn
 import json
 import os
@@ -9,57 +10,54 @@ import numpy as np
 from PIL import ImageDraw, Image
 import random
 
-
 def generate_noisy_indexes(seed, dataset_size, noisy_rate):
-    """生成需要加噪的图像的index"""
-
+    """Generate indexes of images to add noise"""
+    
     num = int(dataset_size * noisy_rate)
-    random.seed(seed)
+    random.seed(seed)  
     indexes = random.sample(range(0, dataset_size), num)
     return indexes
 
-
 def add_noisy_triangle_label(mask):
-    """在mask上添加噪声标注"""
-
+    """Add noisy triangle annotation on the mask"""
+    
     while True:
         noisy_image = Image.fromarray(np.zeros(mask.size, dtype=np.uint8))
-        # 生成随机的三角形的顶点坐标
+        # Generate random triangle vertex coordinates 
         noisy_points = np.random.randint(0, 256, (3, 2))
-        # 三角形的跨度不能太大
+        
+        # Triangle span cannot be too large
         if (noisy_points.max(axis=0) - noisy_points.min(axis=0)).max() > 75:
             continue
-
+            
         noisy_points = noisy_points.flatten()
         noisy_points = tuple(noisy_points)
         ImageDraw.Draw(noisy_image).polygon(noisy_points, fill=255)
 
-        # 噪声标签的面积不能太大
+        # Noisy label area cannot be too large
         noisy_array = np.asarray(noisy_image) / 255
         if noisy_array.sum() <= 1500 and noisy_array.sum() >= 1000:
-            # 在原图上添加噪声
+            # Add noise to original mask
             ImageDraw.Draw(mask).polygon(noisy_points, fill=255)
             break
 
     return mask
 
-
 def create_directory(dir_path):
-    """创建目录，若存在，则删除后再创建"""
-
+    """Create directory, delete existing one if needed"""
+    
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path)
-        print('Remove {}'.format(dir_path))
+        print('Removed {}'.format(dir_path))
     os.mkdir(dir_path)
-    print('Create {}'.format(dir_path))
-
+    print('Created {}'.format(dir_path))
 
 def process_binary_mask_tensor(mask):
-    """对生成的Tensor类型的mask进行处理"""
+    """Process generated Tensor mask"""
+    
+    assert isinstance(mask, torch.Tensor), 'Input is not Tensor'
 
-    assert isinstance(mask, torch.Tensor), '输入的不是Tensor类型'
-
-    # ToTensor()会自动给数据添加一个channel维度，因为是类别标签，不需要这个维度
+    # ToTensor adds a channel dim, remove for label 
     if len(mask.shape) == 3:
         mask = torch.squeeze(mask, dim=0)
 
@@ -68,63 +66,59 @@ def process_binary_mask_tensor(mask):
 
     return mask
 
-
 def process_multiple_mask_tensor(mask, color_map):
-    """对生成的Tensor类型的mask进行处理"""
+    """Process generated Tensor mask with multiple classes"""
 
-    assert isinstance(mask, torch.Tensor), '输入的不是Tensor类型'
+    assert isinstance(mask, torch.Tensor), 'Input is not Tensor'
 
-    # ToTensor()会自动给数据添加一个channel维度，因为是类别标签，不需要这个维度
+    # ToTensor adds a channel dim, remove for label
     if len(mask.shape) == 3:
         mask = torch.squeeze(mask, dim=0)
 
-    # 如果mask被Resize，修改其中生成的噪声标签，并将数据类型转换为uint8，将其中的255替换为1
+    # If mask resized, modify noise and convert to uint8
     if torch.max(mask).item() <= 1:
-        # ToTensor()会将数据进行max-min归一化，这里进行还原
+        # Restore from normalization 
         mask = (mask * 255).to(torch.uint8)
 
-        # 记录噪声标记的坐标
+        # Record noise coordinate 
         noise_indexes = torch.ones_like(mask, dtype=torch.bool)
 
-        # 去除噪声标记
+        # Remove noise
         for color_value, class_id in color_map.items():
             noise_indexes = noise_indexes & (mask != color_value)
         mask[noise_indexes] = 0
 
-        # 将对应的类别的颜色值替换为其类别id
+        # Replace color values with class ids
         for color_value, class_id in color_map.items():
             mask[mask == color_value] = class_id
-
-        # 因为Resize使用线性插值的缘故，会产生一些少量的现有类别的标记，但是其数量很少，将其过滤掉
+            
+        # Filter small spurious labels
         for class_id in color_map.values():
             if torch.sum(mask == class_id).item() <= 10:
                 mask[mask == class_id] = 0
 
     return mask
-
-
+    
 def is_augmented(path):
-    """从路径判断是否是被增广的数据"""
-
+    """Check if path is for augmented data"""
+    
     augmented_types = ['VerticalFlip', 'HorizontalFlip', 'Transpose', 'RandomRotate90']
     for t in augmented_types:
         if path.__contains__(t):
             return True
     return False
 
-
 def remove_augmented_item(old_items):
-    """删除被数据增强的数据"""
-
+    """Remove augmented data items"""
+    
     new_items = []
     for item in old_items:
         if not is_augmented(item['image_path']):
             new_items.append(item)
     return new_items
 
-
 def make_dataset(mode, base_path, is_contain_augmented_data):
-    """从指定目录下读取用于training、validation的json文件"""
+    """Load json files for training/validation"""
 
     assert mode in ['train', 'val']
 
@@ -135,7 +129,6 @@ def make_dataset(mode, base_path, is_contain_augmented_data):
     image_list = os.listdir(image_path)
     mask_list = os.listdir(mask_path)
     return image_list, mask_list
-
     # json_path = os.path.join(base_path, '{}.json'.format(mode))
     #
     # with open(json_path, 'r') as f:
@@ -146,9 +139,8 @@ def make_dataset(mode, base_path, is_contain_augmented_data):
     #
     # return items
 
-
 def initialize_weights(model):
-    """初始化神经网络中的参数"""
+    """Initialize weights in the model"""
 
     for module in model.modules():
         if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
